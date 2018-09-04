76756fcb623b9da6a7d518f53fe1f2a5d1e0a19e
tri <tri@localhost.localdomain>
2018-09-04 14:19:22 +0100

    added line of sight radar

--- ifs.py
+++ ifs.py
@@ -1,7 +1,8 @@
 #! /usr/bin/env python3
 
 """
-Infinite Fun Space
+Infinmport threading
+ite Fun Space
 
 Tri-Peter Shrive
 tri.shrive@gmail.com
@@ -12,8 +13,10 @@ TODO:
 	* guidance (should compensate for gravity),
 	* collision detection,
 	* radar,
+	* infra-red,
 	* database,
-	* web sockets
+	* web sockets,
+	* ships
 """
 
 import curses
@@ -42,26 +45,44 @@ class Message(enum.Enum):
 	update_Piece = 3
 	move_Order = 4
 
+class Framework():
+	"""
+	"""
+	def __init__(
+		self,
+		max_x: int = 10,
+		max_y: int = 10,
+		max_z: int = 10
+		):
+		self.max_x = max_x
+		self.max_y = max_y
+		self.max_z = max_z
+		self.board_Map = np.zeros((max_x, max_y, max_z), dtype = int)
+		self.pieces_List = list()
+		self.next_ID = 0
+	
 class Piece:
 	"""
 	"""
 	def __init__(
 		self,
-		position: np.array,
-		speed: np.array
+		Framework: Framework,
+		position: np.array
 		):
 		"""
 		"""
 		self.position = position
-		self.speed = speed
+		self.Framework = Framework
+		self.has_destination = False
 
 	def move_Order(
 		self,
-		destination: np.array,
+		destination: np.array
 		) -> None:
 		"""
 		"""
 		self.destination = destination
+		self.has_destination = True 
 
 	def update(
 		self
@@ -75,6 +96,7 @@ class Rocket(Piece):
 	"""
 	def __init__(
 		self,
+		Framework: Framework,
 		position: np.array,
 		speed: np.array,
 		mass: np.float64 = 1.0,
@@ -86,9 +108,10 @@ class Rocket(Piece):
 		"""
 		Piece.__init__(
 			self,
-			position,
-			speed
+			Framework,
+			position
 			)
+		self.speed = speed
 		self.mass = mass
 		self.final_mass = final_mass
 		self.exit_velocity = exit_velocity
@@ -97,17 +120,20 @@ class Rocket(Piece):
 		total_speed = np.sum(np.abs(self.speed))
 		if(0 < total_speed):
 			self.direction = self.speed / float(total_speed)
+
 	def update(
 		self
 		) -> None:
 		"""
+		Newtons method.
+		Should use RK4.
 		"""
 		self.thrust = self.exit_velocity * self.mass_flow_rate
 		assert(0 < self.mass_flow_rate)
 		assert(self.mass_flow_rate < self.mass)
 		self.acceleration = self.thrust / float(self.mass)
 		# do we have a destination?
-		if("destination" in self.__dict__):
+		if(True == self.has_destination):
 			direction = self.destination - self.position
 			sum_direction = np.sum(np.abs(direction))
 			if(0 < sum_direction):
@@ -119,56 +145,98 @@ class Rocket(Piece):
 					self.mass = self.mass - self.mass_flow_rate
 		self.position = self.position + self.speed
 
-class Message_Bus:
+class Radar(Piece):
 	"""
 	"""
 	def __init__(
 		self,
-		Queue,
-		callbacks: list,
-		num_threads: int = 4
+		Framework: Framework,
+		position: np.array,
 		):
 		"""
 		"""
-		self.Queue = Queue
-		self.callbacks = callbacks
-		for i in range(num_threads):
-			worker = threading.Thread(
-					target = self.__get_Message, 
-					args = (self.Queue,)
-					)
-			worker.setDaemon(True)
-			worker.start()
+		Piece.__init__(
+			self,
+			Framework,
+			position
+			)
+		self.visible_pieces = list()
 
-	def __get_Message(
-		self,
-		Queue: queue.Queue
+	def update(
+		self
 		) -> None:
 		"""
 		"""
-		while(True):
-			item = Queue.get()
-			Msg = item[0]
-			args = item[1]
-			for callback in self.callbacks:
-				callback.handle_Message(Msg, args)
-			Queue.task_done()
+		logger.debug(self.position)
+		self.visible_pieces = list()
+		for piece in self.Framework.pieces_List:
+			if(self.is_Visible(piece)):
+				self.visible_pieces.append(piece)
+				logger.debug("self: {}".format(self.position))
+				logger.debug("visible: {}".format(piece.position))
 
-	def post_Message(
+	def is_Visible(
 		self,
-		Msg: Message,
-		args: list = None
+		piece: Piece
+		) -> bool:
+		"""
+		Line of sight.
+		"""
+		distance = np.linalg.norm(piece.position[:2] - self.position[:2])
+
+		piece_height = piece.position[2]
+		self_height =  self.position[2]
+
+		earth_radius = 6371
+		radar_height = 0.001
+		piece_horizon = np.sqrt(2 * earth_radius * piece_height)
+		self_horizon = np.sqrt(2 * earth_radius * (self_height + radar_height) + (self_height + radar_height)**2)
+
+		if(distance < piece_horizon + self_horizon):
+			is_visible = True
+		else:
+			is_visible = False
+	
+		return is_visible
+
+class Ship(Piece):
+	"""
+	"""
+	def __init__(
+		self,
+		Framework: Framework,
+		position: np.array,
+		speed: np.array,
+		rockets: list = list()
+		):
+		"""
+		Carries radars and rockets over seas.
+		"""
+		Piece.__init__(
+			self,
+			Framework,
+			position
+			)
+		self.speed = speed
+		self.rockets = rockets
+		self.radar = Radar(
+				Framework,
+				position
+				)
+
+	def update(
+		self
 		) -> None:
 		"""
 		"""
-		self.Queue.put((Msg, args))
+		self.radar.update()
 
 class System:
 	"""
 	"""
 	def __init__(
 		self,
-		Framework,
+		Framework: Framework,
 		Queue: queue.Queue
 		):
 		"""
@@ -194,6 +262,42 @@ class System:
 		"""
 		pass
 
+class Message_Bus(System):
+	"""
+	"""
+	def __init__(
+		self,
+		Framework,
+		Queue,
+		callbacks: list,
+		num_threads: int = 4
+		):
+		"""
+		"""
+		System.__init__(self, Framework, Queue)
+		self.callbacks = callbacks
+		for i in range(num_threads):
+			worker = threading.Thread(
+					target = self.__get_Message, 
+					args = (self.Queue,)
+					)
+			worker.setDaemon(True)
+			worker.start()
+
+	def __get_Message(
+		self,
+		Queue: queue.Queue
+		) -> None:
+		"""
+		"""
+		while(True):
+			item = Queue.get()
+			Msg = item[0]
+			args = item[1]
+			for callback in self.callbacks:
+				callback.handle_Message(Msg, args)
+			Queue.task_done()
+
 class Interface(System):
 	"""
 	"""
@@ -205,6 +309,20 @@ class Interface(System):
 		"""
 		"""
 		System.__init__(self, Framework, Queue)
+
+	def handle_Message(
+		self,
+		Msg: Message,
+		args: list = None
+		) -> None:
+		"""
+		"""
+		if(Message.add == Msg):
+			self.add(args)
+		elif(Message.move_Order == Msg):
+			self.move_Order(args)
+		else:
+			pass
 
 	def open_Window(
 		self
@@ -279,6 +397,37 @@ class Interface(System):
 		window_size = stdscr.getmaxyx()
 		stdscr.move(window_size[0] - 1, 0)
 
+	def move_Order(
+		self,
+		args
+		) -> None:
+		"""
+		"""
+		old_x = int(args[0])
+		old_y = int(args[1])
+		old_z = int(args[2])
+
+		new_x = int(args[3])
+		new_y = int(args[4])
+		new_z = int(args[5])
+		
+		piece_ID = Framework.board_Map[old_x, old_y, old_z]
+		destination = np.array((new_x, new_y, new_z), dtype = int)
+		Framework.pieces_List[piece_ID].move_Order(destination)
+
+	def add(
+		self,
+		args
+		) -> None:
+		"""
+		"""
+		position = np.array((args[0], args[1], args[2]), dtype = np.float64)
+		speed = np.array((args[3], args[4], args[5]), dtype = np.float64)
+	
+		Framework.pieces_List.append(Ship(Framework, position, speed))
+		Framework.board_Map[int(args[0]), int(args[1]), int(args[2])] = Framework.next_ID
+		Framework.next_ID = Framework.next_ID + 1
+		
 class Logic(System):
 	"""
 	"""
@@ -298,47 +447,12 @@ class Logic(System):
 		) -> None:
 		"""
 		"""
-		if(Message.add == Msg):
-			self.add(args)
-		elif(Message.update_Board == Msg):
+		if(Message.update_Board == Msg):
 			self.update_Board()
 		elif(Message.update_Piece == Msg):
 			self.update_Piece(args)
-		elif(Message.move_Order == Msg):
-			self.move_Order(args)
 		else:
 			pass
-
-	def add(
-		self,
-		args
-		) -> None:
-		"""
-		"""
-		position = np.array((np.rint(args[0]), np.rint(args[1]), np.rint(args[2])), dtype = np.float64)
-		speed = np.array((np.rint(args[3]), np.rint(args[4]), np.rint(args[5])), dtype = np.float64)
-	
-		Framework.pieces_List.append(Rocket(position, speed))
-		Framework.board_Map[int(np.rint(args[0])), int(np.rint(args[1])), int(np.rint(args[2]))] = Framework.next_ID
-		Framework.next_ID = Framework.next_ID + 1
-		
-	def move_Order(
-		self,
-		args
-		) -> None:
-		"""
-		"""
-		old_x = int(args[0])
-		old_y = int(args[1])
-		old_z = int(args[2])
-
-		new_x = int(args[3])
-		new_y = int(args[4])
-		new_z = int(args[5])
-		
-		piece_ID = Framework.board_Map[old_x, old_y, old_z]
-		destination = np.array((new_x, new_y, new_z), dtype = int)
-		Framework.pieces_List[piece_ID].move_Order(destination)
 
 	def update_Board(
 		self,
@@ -350,43 +464,19 @@ class Logic(System):
 
 	def update_Piece(
 		self,
-		args
+		piece
 		) -> None:
 		"""
 		"""
-		args.update()
+		piece.update()
 
-
-class Framework():
-	"""
-	"""
-	def __init__(
-		self,
-		max_x: int = 10,
-		max_y: int = 10,
-		max_z: int = 10
-		):
-		self.max_x = max_x
-		self.max_y = max_y
-		self.max_z = max_z
-		self.board_Map = np.zeros((max_x, max_y, max_z), dtype = int)
-		self.pieces_List = list()
-		self.next_ID = 0
-	
 if(__name__ == "__main__"):
 	Framework = Framework()
 	Queue = queue.Queue(maxsize = 0)
 	Logic = Logic(Framework, Queue)
 	Interface = Interface(Framework, Queue)
-	Message_Bus = Message_Bus(Queue, [Logic, Interface])
-	Message_Bus.post_Message(Message.add, [0, 0, 0, 0, 0, 0])
-	Message_Bus.post_Message(Message.add, [0, 8, 0, 0, 0, 0])
-	Message_Bus.post_Message(Message.add, [8, 0, 0, 0, 0, 0])
-	Message_Bus.post_Message(Message.add, [8, 8, 0, 0, 0, 0])
-	Queue.join()
-	Message_Bus.post_Message(Message.move_Order, [0, 0, 0, 4, 4, 4])
-	Message_Bus.post_Message(Message.move_Order, [0, 8, 0, 4, 4, 4])
-	Message_Bus.post_Message(Message.move_Order, [8, 0, 0, 4, 4, 4])
-	Message_Bus.post_Message(Message.move_Order, [8, 8, 0, 4, 4, 4])
+	Message_Bus = Message_Bus(Framework, Queue, [Logic, Interface])
+	Message_Bus.post_Message(Message.add, [1, 8, 0, 0, 0, 0])
+	Message_Bus.post_Message(Message.add, [8, 1, 0, 0, 0, 0])
 	Queue.join()
 	Interface.open_Window()
